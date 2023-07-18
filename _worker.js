1186b0750863da16267d1be52a1324a7090a5aed
soyoung1980 <130219832+soyoung1980@users.noreply.github.com>
2023-07-19 00:51:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ddd3a820-e3e8-4981-8071-034579632f5c';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org';
+let proxyIP = 'edgetunnel.anycast.eu.org';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
